336f87db9e2acea215ed252b0fc6f602bd5c2d36
pperezp <patodeath@gmail.com>
2017-09-13 23:23:57 -0300

    Primera vez subiendo el proyecto. Aún falta mejorar el código y añadir nuevas ideas

---
+++ Test_BKN/Test_BKN/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Test_BKN {
+    partial class Form1 {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.comando = new System.Windows.Forms.TextBox();
+            this.lbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comando
+            // 
+            this.comando.Location = new System.Drawing.Point(12, 25);
+            this.comando.Name = "comando";
+            this.comando.Size = new System.Drawing.Size(328, 20);
+            this.comando.TabIndex = 0;
+            this.comando.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_KeyPress);
+            this.comando.KeyUp += new System.Windows.Forms.KeyEventHandler(this.comando_KeyUp);
+            // 
+            // lbl
+            // 
+            this.lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl.ForeColor = System.Drawing.Color.Red;
+            this.lbl.Location = new System.Drawing.Point(13, 48);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(328, 267);
+            this.lbl.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ESCRIBA: gran bkn escucha nuestras plegarias";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 324);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.comando);
+            this.Name = "Form1";
+            this.Text = "GRAN BKN";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox comando;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Test_BKN/Test_BKN/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_BKN {
+    public partial class Form1 : Form {
+
+        public delegate void delUpdateUILabel(String text);
+
+        private String frase = "BKN respondeme por favor";
+        private String respuesta;
+        private Boolean inicio;
+        private Boolean pregunta;
+
+
+        public Form1() {
+            InitializeComponent();
+            inicio = false;
+            respuesta = "";
+            pregunta = true;
+            label1.Text = frase;
+        }
+
+        private char getLetra(int indice) {
+            return frase[indice];
+        }
+
+        private void txt_KeyPress(object sender, KeyPressEventArgs e) {
+            if (e.KeyChar == '.') {
+                inicio = !inicio;
+                procesar(e);
+            } else if (inicio) {
+                procesar(e);
+
+                respuesta += e.KeyChar;
+            }
+        }
+
+        private void procesar(KeyPressEventArgs e) {
+            comando.Text += getLetra(comando.Text.Length);
+            e.Handled = true;// el consume() de java
+            comando.SelectionStart = comando.Text.Length;
+        }
+
+        private void comando_KeyUp(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Enter) {
+                if (pregunta) {// enter en la pregunta
+                    pregunta = false;
+                    enviarMensaje("Dime hijo mio.", false);
+                } else {// enter en la respuesta
+                    pregunta = true;
+                    enviarMensaje(respuesta, true);
+                }
+                comando.Text = "";
+                comando.Focus();
+            }
+        }
+
+        private void enviarMensaje(String mensaje, Boolean resp) {
+            lbl.Text = "";
+            this.Invoke((MethodInvoker)async delegate {
+                for (int i = 0; i < 3; i++) {
+                    lbl.Text += " . ";
+                    await Task.Delay(1000);
+                }
+                lbl.Text += "\n";
+                if (resp) {
+                    String buscando = "Buscando en la nasa...";
+                    foreach (char letra in buscando) {
+                        lbl.Text += letra;
+                        await Task.Delay(50);
+                    }
+                    lbl.Text += "\n";
+                    for (int i = 0; i < 3; i++) {
+                        lbl.Text += " . ";
+                        await Task.Delay(1000);
+                    }
+                }
+
+                lbl.Text += "\n";
+                foreach (char letra in mensaje) {
+                    lbl.Text += letra;
+                    await Task.Delay(200);
+                }
+            });
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            new TestHilo().Show();
+        }
+    }
+}
